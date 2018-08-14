0356108eedfe7e62996bd4bc59bd3c0789dd41e0
ayoubhamaoui <ayoub10hamaoui@gmaiL.com>
2018-08-14 03:39:03 +0100

    server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose= require('mongoose');
+
+
+const app=express();
+
+//connecting to mLab database
+mongoose.connect('mongodb://ayoub:hamaoui10@ds219832.mlab.com:19832/kickestate')
+mongoose.connection.once('open',()=>{
+    console.log('connected to kickstate database !!');
+});
+
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}));
+
+app.listen(4000,()=>{
+    console.log('now lestening for request on port 4000');
+});
