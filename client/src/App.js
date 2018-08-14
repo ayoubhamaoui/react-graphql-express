410d27a5652e2450e73d0d2eaae3bde1b1cbbef2
Ayoub Hamaoui <ayoub10hamaoui@gmail.com>
2018-08-14 06:23:16 +0100

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+//import components
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>GraphQL Ayoub</h1>
+        <BookList/>
       </div>
     );
   }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+
+
 class BookList extends Component {
   render() {
     return (
       <div>
         <ul id="book-list">
-          <h1>Reading List</h1>
+          <li>Book name</li>
+
         </ul>
       </div>
     );
