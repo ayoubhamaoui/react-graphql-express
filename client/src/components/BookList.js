1cb571aaa536f438af7d81a1848cf60b1f2d80d1
Ayoub Hamaoui <ayoub10hamaoui@gmail.com>
2018-08-14 06:18:44 +0100

    client

---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <h1>Reading List</h1>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
